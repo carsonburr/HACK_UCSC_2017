eedc958eacbf570cb4b6be494abc3effe10e2611
carsonburr <carsonwburr@gmail.com>
2017-01-21 02:18:01 -0800

    Boring stuff out of the way. Implemented FlowRouter. Let's do this.

---
+++ imports/startup/client/router.js
@@ -0,0 +1,26 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+import '../../ui/layout/app_body.js';
+
+import '../../ui/pages/root_redirector.js';
+
+FlowRouter.route('/home', {
+	name: 'App.home_page',
+	action() {
+		BlazeLayout.render('App_body', { main: 'app_home_page' });
+	},
+});
+
+FlowRouter.route('/', {
+	name: 'App.home',
+	action() {
+		BlazeLayout.render('App_body', { main: 'app_root_redirector' });
+	},
+});
+
+FlowRouter.notFound = {
+	action() {
+		BlazeLayout.render('App_body', {});
+	}
+};
